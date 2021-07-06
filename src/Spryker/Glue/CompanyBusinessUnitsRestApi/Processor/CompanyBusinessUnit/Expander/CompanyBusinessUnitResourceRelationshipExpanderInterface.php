f4a68a27301c49342d4fd5ad92c17bd8936c52fe
Alexander Veselov <alexander.veselov@spryker.com>
2021-07-06 16:27:56 +0300

    GLUE-10972: API: Create Request for Quote (#8241)
    GLUE-10972 API: Create Request for Quote

--- src/Spryker/Glue/CompanyBusinessUnitsRestApi/Processor/CompanyBusinessUnit/Expander/CompanyBusinessUnitResourceRelationshipExpanderInterface.php
+++ src/Spryker/Glue/CompanyBusinessUnitsRestApi/Processor/CompanyBusinessUnit/Expander/CompanyBusinessUnitResourceRelationshipExpanderInterface.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Glue\CompanyBusinessUnitsRestApi\Processor\CompanyBusinessUnit\Relationship;
+namespace Spryker\Glue\CompanyBusinessUnitsRestApi\Processor\CompanyBusinessUnit\Expander;
 
 use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
 
